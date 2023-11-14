b0d1274deeabb1d2f1d013f37c59cd31a12a2603
WonhwaGal <110241714+WonhwaGal@users.noreply.github.com>
2023-11-14 21:56:34 +0300

    Prefabs

---
+++ Assets/Scripts/MVP/Registry/ConstructionRegistry.cs
@@ -0,0 +1,30 @@
+using Code.Pools;
+using Code.ScriptableObjects;
+using Code.Strategy;
+
+namespace Code.Construction
+{
+    public sealed class ConstructionRegistry
+    {
+        private readonly MultiPool<PrefabType, ConstructionView> _multiPool;
+        private readonly StrategyHandler _strategyHandler;
+
+        public ConstructionRegistry(MultiPool<PrefabType, ConstructionView> pool)
+        {
+            _multiPool = pool;
+            _strategyHandler = ServiceLocator.Container.RequestFor<StrategyHandler>();
+        }
+
+        public ConstructionPresenter CreatePresenter(SingleBuildingData buildingData)
+        {
+            var buildingView = _multiPool.Spawn(buildingData.PrefabType);
+            _multiPool.OnSpawned(buildingView, buildingData);
+            var model = new ConstructionModel(buildingData);
+            var strategy = 
+                (IConstructionStrategy)_strategyHandler.GetStrategy(buildingData.PrefabType);
+
+            var presenter = new ConstructionPresenter(buildingView, model, strategy);
+            return presenter;
+        }
+    }
+}
